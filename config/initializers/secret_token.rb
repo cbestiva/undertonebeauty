d2bcffbf4b6be892d526c6471933ad3280a6f377
cbestiva <cbestiva@gmail.com>
2014-06-06 17:56:13 -0700

    created rails app with postgres db and added test development gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Undertonebeauty::Application.config.secret_key_base = '47ce6d0e9a28b9ea39225adcc8cb75fc90cebeb9ea2cbb8fcaaf09af9c355dee96c31c81803a7f4e09fef6ed8998eeff7e935a4321d4cca4f4d7a57860cc49cb'
