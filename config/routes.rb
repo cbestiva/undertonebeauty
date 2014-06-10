c6627cac5320f63ae15ab2da2215e74bef969e0d
cbestiva <cbestiva@gmail.com>
2014-06-09 18:24:58 -0700

    added user controller, profile page, and profile page redirect after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Undertonebeauty::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations", passwords: "users/passwords" }
   root to: "sites#index"
 
   get "/undertonebeauty", to: "sites#index", as: :home_url
+  get "users/show/:id" => "users#show", as: "profile"
 end
