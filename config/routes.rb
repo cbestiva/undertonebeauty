f97771a2714e3f55e50c7cb6d01cf5a5101c156b
cbestiva <cbestiva@gmail.com>
2014-06-09 21:56:48 -0700

    fixed routing error for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Undertonebeauty::Application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations", passwords: "users/passwords" }
+  devise_for :users
   root to: "sites#index"
 
   get "/undertonebeauty", to: "sites#index", as: :home_url
