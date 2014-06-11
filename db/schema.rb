09e15cb559ade6e32e2ee6d1713415d0d407a634
cbestiva <cbestiva@gmail.com>
2014-06-11 16:06:34 -0700

    generated product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610232230) do
+ActiveRecord::Schema.define(version: 20140611230350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "eyecolors", force: true do |t|
     t.string   "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "review"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
