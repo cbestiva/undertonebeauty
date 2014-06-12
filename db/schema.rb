9e2425062666405a4867adfda7a2acaca780dc5d
cbestiva <cbestiva@gmail.com>
2014-06-11 20:16:33 -0700

    generated category model and added its association to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612003317) do
+ActiveRecord::Schema.define(version: 20140612030204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "categories", ["product_id"], name: "index_categories_on_product_id", using: :btree
 
   create_table "eyecolors", force: true do |t|
     t.string   "color"
