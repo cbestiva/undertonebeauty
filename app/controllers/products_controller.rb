861c813ad7bb7680216b1e43921714bda3956c6f
cbestiva <cbestiva@gmail.com>
2014-06-11 22:17:43 -0700

    user able to create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,25 @@
 class ProductsController < ApplicationController
+  before_filter :authenticate_user!, only: [:create, :update, :destroy]
   def index
     @products = Product.all
+
+    respond_to do |f|
+      f.html
+      f.json {render json: @products}
+    end
   end
 
-  def show    
+  def create
+    new_product = params.require(:product).permit(:name, :category_id, :review)
+    @product = current_user.products.create(new_product)
+
+    respond_to do |f|
+      f.html {redirect_to(profile_url(current_user))}
+      f.json {render json: @product}
+    end
+  end
+
+  def show
+    @products = current_user.products 
   end
 end
