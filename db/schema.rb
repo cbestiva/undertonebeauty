c1b688c39d3a9b9e87cd0b83cda5c5fc96ab1dcc
cbestiva <cbestiva@gmail.com>
2014-06-11 20:38:07 -0700

    added category to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612030204) do
+ActiveRecord::Schema.define(version: 20140612033446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20140612030204) do
     t.text     "review"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "skintones", force: true do |t|
     t.string   "color"
