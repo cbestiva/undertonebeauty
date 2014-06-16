4d3ffb1e70d87abdce0997ad933fd24f673dd023
cbestiva <cbestiva@gmail.com>
2014-06-15 19:12:50 -0700

    added image to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614044135) do
+ActiveRecord::Schema.define(version: 20140616021213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140614044135) do
     t.datetime "updated_at"
     t.integer  "category_id"
     t.integer  "user_id"
+    t.string   "image"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
