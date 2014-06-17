116a7c0a314e026416164fef4f958985a6d5e453
cbestiva <cbestiva@gmail.com>
2014-06-17 15:51:16 -0700

    cleaned up code for querying user

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,18 +3,39 @@ class SitesController < ApplicationController
   end
 
   def browndark
-    @users = User.find(:all, order: :username)
+    @users = User.where( {skintone_id: [5,6]} ).order(:username)
+
+    respond_to do |f|
+      f.html
+      f.json {render json: @users}
+    end
+
   end
 
   def fairmedium
-    @users = User.find(:all, order: :username)
+    @users = User.where( {skintone_id: [2,3]} ).order(:username)
+
+    respond_to do |f|
+      f.html
+      f.json {render json: @users}
+    end
   end
 
   def olivebrown
-    @users = User.find(:all, order: :username)
+    @users = User.where( {skintone_id: [4,5]} ).order(:username)
+
+    respond_to do |f|
+      f.html
+      f.json {render json: @users}
+    end
   end
 
   def lightfair
-    @users = User.find(:all, order: :username)
+    @users = User.where( {skintone_id: [1,2]} ).order(:username)
+
+    respond_to do |f|
+      f.html
+      f.json {render json: @users}
+    end
   end
 end
