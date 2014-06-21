11449e798eae3b23baafcf490bfbba04193ae524
cbestiva <cbestiva@gmail.com>
2014-06-20 21:03:34 -0700

    added working javascript filter for olive-brown skintone views

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Undertonebeauty::Application.routes.draw do
   get "undertonebeauty/fair-medium" => "sites#fairmedium"
   post "undertonebeauty/fair-medium" => "sites#fairmedium"
   get "undertonebeauty/olive-brown" => "sites#olivebrown"
+  post "undertonebeauty/olive-brown" => "sites#olivebrown"
   get "undertonebeauty/light-fair" => "sites#lightfair"
   get "undertonebeauty/:username/:id" => "users#preview"
 end
